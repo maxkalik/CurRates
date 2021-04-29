345fa166c7e63172c3b9883fdd4e3713bb791b2b
Maksim Kalik <maxkalik@gmail.com>
2021-04-29 23:23:59 +0300

    fixed minor paddings

--- Shared/Views/MiniRatesView.swift
+++ Shared/Views/MiniRatesView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct MiniRatesView: View {
     private(set) var buy: String
     private(set) var sell: String
-    private(set) var reversed: Bool?
+    private(set) var reversed: Bool
     
-    init(buy: String, sell: String, reversed: Bool? = nil) {
+    init(buy: String, sell: String, reversed: Bool = false) {
         self.buy = buy
         self.sell = sell
         self.reversed = reversed
@@ -21,9 +21,11 @@ struct MiniRatesView: View {
     var body: some View {
         if !(buy.isEmpty && sell.isEmpty) {
             VStack {
-                RateItemView(title: "Sell", value: sell, reversed: reversed ?? false)
+                RateItemView(title: "Sell", value: sell, reversed: reversed)
                     .padding(.bottom, 1)
-                RateItemView(title: "Buy", value: buy, reversed: reversed ?? false)
+                    .padding(.trailing, reversed ? 5 : 0)
+                RateItemView(title: "Buy", value: buy, reversed: reversed)
+                    .padding(.trailing, reversed ? 5 : 0)
             }
         } else {
             Text("—")
